fa60f8ef04c2c5bfad0f482734ff2f977fd38ebd
ryan2414 <75060346+ryan2414@users.noreply.github.com>
2022-04-13 21:15:07 +0900

    Finish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,11 +43,17 @@ public class GameManager : MonoBehaviour {
 
     // 점수를 증가시키는 메서드
     public void AddScore(int newScore) {
-        
+        if (!isGameover)
+        {
+            //점수를 증가
+            score += newScore;
+            scoreText.text = "Score: " + score;
+        }
     }
 
     // 플레이어 캐릭터가 사망시 게임 오버를 실행하는 메서드
     public void OnPlayerDead() {
-        
+        isGameover = true;
+        gameoverUI.SetActive(true);
     }
 }
